863ede8c89d9c1a967153cfef8366c051cd02ace
St.Onge <stonge.ms@gmail.com>
2021-06-16 10:05:34 -0700

    Updated the wind speed and cloud cover

--- HW6/script.js
+++ HW6/script.js
@@ -33,15 +33,15 @@ faSearchBtn.addEventListener("click", function(){
         citySaveResults.push(searchBarText.value)
         localStorage.setItem("cities", JSON.stringify(citySaveResults))
 
-        var iconcode = data.weather[0].icon;
-        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        let iconcode = data.weather[0].icon;
+        let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
         weatherDiv.innerHTML = `
          <h2 class="city">${data.name}</h2>
          <div class="temp">${Math.round(data.main.temp)}°</div>
          <img class="icon" src="${iconurl}" alt="" />
-         <div class="description">${data.name}</div>
+         <div class="description">${data.weather[1]}</div>
          <div class="humidity">${data.main.humidity}% Humidity</div>
-         <div class="wind">${data.name}</div>
+         <div class="wind">${Math.round(data.wind.speed)}mph</div>
         `
       });
 });
@@ -50,21 +50,3 @@ faSearchBtn.addEventListener("click", function(){
 
 
 
-
-// onst getWeather = function (city) {
-//     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e6989dabdc4acd8059acb7786b6dfb7c`;
-//     return fetch(apiUrl)
-//       .then(function (response) {
-//         if (response.ok) {
-//           response.json().then(function (data) {
-//             displayWeather(data);
-//           });
-//         } else {
-//           data;
-//           alert("Error: " + response.statusText);
-//         }
-//       })
-//       .catch(function (error) {
-//         alert("Unable to gather weather data!");
-//       });
-//   };
